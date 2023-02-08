b82a62960b0ac4ae01d7270d528d5f2807197d69
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-02-08 10:57:58 -0500

    Add 404 handling

--- pages/[[...ssr]].tsx
+++ pages/[[...ssr]].tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Error from 'next/error'
 import Common, { CommonProps } from "../components/common"
 import Bowser from "bowser";
 import { SWRConfig, unstable_serialize } from 'swr'
@@ -10,10 +11,10 @@ import {
 } from '../lib/lakeApi';
 import { withSessionSsr } from '../lib/withSession';
 import { fetchQueues } from '../lib/ssrQueueFetches';
-import {Options} from '../lib/withSession';
+import { Options } from '../lib/withSession';
 import Config from '../lib/config';
 import shallowEqual from '../lib/shallowEqual';
-
+import { Qparams } from '../lib/qparams'
 import {
     GetServerSidePropsContext,
     GetServerSidePropsResult,
@@ -22,10 +23,27 @@ import {
 import { stringify } from "querystring";
 import { networkInterfaces } from "os";
 
+interface HomeProps {
+    session?: Options,
+    qparams?: Qparams,
+    fallback?: any,
+    meta?: {
+        description?: string,
+        title?: string,
+        site_name?: string,
+        image?: string,
+        publishedTime?: number;
+        url?: string;
 
-export default function Home({ session, qparams, fallback,meta }: CommonProps) {
-
-    return <SWRConfig value={{ fallback }}><Common session={session} qparams={qparams} meta={meta} /></SWRConfig>
+    },
+    error?: number
+}
+export default function Home({ session, qparams, fallback, meta, error }: HomeProps) {
+    if (error)
+        return <Error statusCode={error}>Ooops, ${error}</Error>
+    if (session && qparams && meta)
+        return <SWRConfig value={{ fallback }}><Common session={session} qparams={qparams} meta={meta} /></SWRConfig>
+    return undefined;
 }
 
 /**
@@ -40,25 +58,25 @@ export default function Home({ session, qparams, fallback,meta }: CommonProps) {
 
 export const getServerSideProps = withSessionSsr(
     async function getServerSideProps(context: GetServerSidePropsContext) {
-      
-        const host=context.req.headers.host;
-        console.log("HOST:",host)
-        const {code,state}:{code:string,state:string}=context.query as any;
+
+        const host = context.req.headers.host;
+        console.log("HOST:", host)
+        const { code, state }: { code: string, state: string } = context.query as any;
         // parse dynamic params:
         let ssr = context.params?.ssr as string[];
-        if(!ssr)
+        if (!ssr)
             ssr = [`${process.env.DEFAULT_FORUM}`];
         let [forum] = ssr;
-       
+
         console.log("FORUM:", forum)
-        if(forum=='index'){
-            console.log("INDEX FORUM")
-            forum=process.env.DEFAULT_FORUM||'';
+        if (forum != 'usconservative') {
+            context.res.statusCode = 404;
+            return { props: { error: 404 } }
         }
         let type = ssr[1];
         if (!type)
             type = 'newsline';
-      //  console.log("TYPE:", type)
+        //  console.log("TYPE:", type)
         const tag = (type == 'topic' || type == 'home' || type == 'solo') ? ssr[2] : "";
         const threadid = (type == 'topic' || type == 'home') ? ssr[3] : "";
         let layoutNumber = ((type == 'topic' || type == 'home') ? ssr[4] : type == 'solo' ? ssr[3] : ssr[2]) || "l1";
@@ -81,7 +99,7 @@ export const getServerSideProps = withSessionSsr(
 
 
         // get encrypted session from the cookie or initialize the default   
-        let startoptions = context.req.session?.options||null;
+        let startoptions = context.req.session?.options || null;
         // console.log("SSR. gpt session options", options)
 
         if (!startoptions) {
@@ -103,8 +121,8 @@ export const getServerSideProps = withSessionSsr(
 
             await context.req.session.save();
         }
-        let options:Options=startoptions;
-      
+        let options: Options = startoptions;
+
         if (options && !options.width)
             options.width = defaultWidth;
         if (typeof options.userslug === 'undefined')
@@ -130,67 +148,67 @@ export const getServerSideProps = withSessionSsr(
         }
         //const isFirstServerCall = context.req?.url?.indexOf('/_next/data/') === -1
         if (code) {
-            console.log("CODE: ",code,state);
-            const jsonState=JSON.parse(state);
-            const {href,appid}=jsonState;
-            console.log("CODE: ",code,state,href,appid);
-            const  user= await processLoginCode(code,appid);
+            console.log("CODE: ", code, state);
+            const jsonState = JSON.parse(state);
+            const { href, appid } = jsonState;
+            console.log("CODE: ", code, state, href, appid);
+            const user = await processLoginCode(code, appid);
             if (user) {
                 const { slug } = user;
                 console.log("ffff1")
-              //  axios.post(`/api/session/save`, { session: { userslug: slug } });
+                //  axios.post(`/api/session/save`, { session: { userslug: slug } });
                 if (context.req.session.options) {
                     console.log("context.req.session.option")
                     context.req.session.options.userslug = slug;
                     await context.req.session.save();
 
                     const userSession = await initLoginSession(slug, options)
-                    
+
                     if (userSession) {
-                        options = userSession||options;
-                        if(options)
-                        context.req.session.options = options;
+                        options = userSession || options;
+                        if (options)
+                            context.req.session.options = options;
                         await context.req.session.save();
                     }
                 }
-                console.log("redirecting after code",href)
+                console.log("redirecting after code", href)
                 return {
                     redirect: {
-                      permanent: false,
-                      destination: href,
+                        permanent: true,
+                        destination: href,
                     },
-                    props:{},
-                  };
+                    props: {},
+                };
             }
-           
+
             //context.res.writeHeader(301,{Location:href} );
         }
         else if (options.userslug) {
             const userSession = await getUserSession(options.userslug)
-            if (userSession&&!shallowEqual(options, userSession)) {
-                options=userSession;
+            if (userSession && !shallowEqual(options, userSession)) {
+                options = userSession;
                 console.log("USER SESSION IS DIFFERENT FROM LOCAL")
-                if(options)
-                context.req.session.options = options;
+                if (options)
+                    context.req.session.options = options;
                 await context.req.session.save();
             }
         }
 
         const channelConfig = await fetchChannelConfig(newsline);
-       
-        console.log("GOT channelConfig",channelConfig)
-        const layoutType = type == 'topic' ? 'context' :type=='solo'?'newsline':type;
+
+        console.log("GOT channelConfig", channelConfig)
+        const layoutType = type == 'topic' ? 'context' : type == 'solo' ? 'newsline' : type;
         const key: fetchChannelLayoutKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, options.dense, options.thick, layoutNumber];
         //console.log("CALLING fetchChannelLayout:",key);
         const channelLayout = await fetchChannelLayout(key);
         console.log("GOT CHANNEL LAYOUT", JSON.stringify(channelLayout))
-       // console.log("=================")
-        const staleLKey= ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 0,1, layoutNumber];
-        const staleLKey1= ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 0,0, layoutNumber];
-        const staleLKey2= ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 1,0, layoutNumber];
-        const staleLKey3= ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 1,1, layoutNumber];
-        
-        
+        // console.log("=================")
+        const staleLKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 0, 1, layoutNumber];
+        const staleLKey1 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 0, 0, layoutNumber];
+        const staleLKey2 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 1, 0, layoutNumber];
+        const staleLKey3 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, layoutType, 1, 1, layoutNumber];
+
+
         const user = await fetchUser(['user', options.userslug])
 
         let fallback = {
@@ -199,40 +217,40 @@ export const getServerSideProps = withSessionSsr(
             [unstable_serialize(staleLKey)]: channelLayout, // to provide SWR with stale date to avoid flashing loading screen
             [unstable_serialize(staleLKey1)]: channelLayout,
             [unstable_serialize(staleLKey2)]: channelLayout,
-            [unstable_serialize(staleLKey3)]: channelLayout,    
-            [unstable_serialize(['user', options?options.userslug:''])]: user
+            [unstable_serialize(staleLKey3)]: channelLayout,
+            [unstable_serialize(['user', options ? options.userslug : ''])]: user
         }
 
-        let meta:{
-            description?:string,
-            title?:string,
-            site_name?:string,
-            image?:string,
-            publishedTime?:number,
-            url?:string;
-            
-        }={};
+        let meta: {
+            description?: string,
+            title?: string,
+            site_name?: string,
+            image?: string,
+            publishedTime?: number,
+            url?: string;
+
+        } = {};
         if (type == 'topic') {
-            const key: [u: string, threadid: string, withBody: number,userslug:string] = ['topic', threadid, 1,options.userslug];
+            const key: [u: string, threadid: string, withBody: number, userslug: string] = ['topic', threadid, 1, options.userslug];
             const topic = await fetchTopic(key);
-            console.log("GOT TOPIC:",JSON.stringify(topic))
+            console.log("GOT TOPIC:", JSON.stringify(topic))
             fallback[unstable_serialize(key)] = topic;
-            const {item}=topic;
-            meta.description=item.description;
-            meta.site_name=item.site_name;
-            meta.title=`${item.catName}: ${item.title}`;
-            meta.image=item.image;
-            meta.publishedTime=item.shared_time;
-            meta.url=item.shared_time;
+            const { item } = topic;
+            meta.description = item.description;
+            meta.site_name = item.site_name;
+            meta.title = `${item.catName}: ${item.title}`;
+            meta.image = item.image;
+            meta.publishedTime = item.shared_time;
+            meta.url = item.shared_time;
         }
         else {
-            meta.description=channelConfig.channelDetails.description;
-            meta.site_name=channelConfig.channelDetails.displayName;
-            meta.title=channelConfig.channelDetails.displayName
-            meta.image==channelConfig.channelDetails.logo;
-            meta.publishedTime=Date.now()/1000|0; 
+            meta.description = channelConfig.channelDetails.description;
+            meta.site_name = channelConfig.channelDetails.displayName;
+            meta.title = channelConfig.channelDetails.displayName
+            meta.image == channelConfig.channelDetails.logo;
+            meta.publishedTime = Date.now() / 1000 | 0;
         }
-        console.log("META",type,JSON.stringify(meta))
+        console.log("META", type, JSON.stringify(meta))
         const propsWrap = {
             props: {
                 session: options,

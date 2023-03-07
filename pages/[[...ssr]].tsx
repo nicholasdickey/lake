f609bcbedba5e0ac433f1fc6ace50aa702528605
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-03-07 03:53:25 -0500

    Code review root ssr page

--- pages/[[...ssr]].tsx
+++ pages/[[...ssr]].tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import Error from 'next/error'
-
 import Common from "../components/common"
 import {GetServerSidePropsContext} from "next";
 import { SWRConfig, unstable_serialize } from 'swr'
 import Bowser from "bowser";
-
 import {
     fetchChannelConfig, fetchChannelLayout, fetchUser,fetchSitemap,
     fetchAllSitemaps,fetchChannelLayoutKey, fetchTopic, FetchTopicKey, processLoginCode, initLoginSession, getUserSession
@@ -29,6 +27,7 @@ interface HomeProps {
     },
     error?: number
 }
+//All root presentation logic for broasheet pages is handled in Common component:
 export default function Home({ session, qparams, fallback, meta, error }: HomeProps) {
     if (error)
         return <Error statusCode={error}>Ooops, ${error}</Error>
@@ -51,7 +50,7 @@ export const getServerSideProps = withSessionSsr(
     async function getServerSideProps(context: GetServerSidePropsContext): Promise<any> {
         let host = context.req.headers.host || "";
         
-        //TODO: add a header to the load balancer to pass correct host
+        //TODO: add a header to the load balancer to pass the correct host
         if(host=='cloud.digitalocean.com')
             host='american-outdoorsman.news';
         
@@ -89,36 +88,31 @@ export const getServerSideProps = withSessionSsr(
             context.res.statusCode = 404;
             return { props: { error: 404 } }
         }
+        
         let type = ssr[1]||'newsline';
         
         const tag = (type == 'topic' || type == 'home' || type == 'solo') ? ssr[2] : "";
-        let threadid = (type == 'topic' || type == 'home') ? ssr[3] : "";
-        if (!threadid)
-            threadid = '';
-        let layoutNumber = ((type == 'topic' || type == 'home') ? ssr[4] : type == 'solo' ? ssr[3] : ssr[2]) || "l1";
-        let navTab = ((type == 'newsline') ? ssr[3] : type == "solo" ? ssr[4] : 0) || 1;
-        let cc = (type == 'topic' || type == 'home') ? ssr[5] : '';
-        if (!cc)
-            cc = '';
-          console.log("SSR args:", JSON.stringify({forum,type,threadid,tag,layoutNumber}))
-
-        let ua = context.req.headers['user-agent'];
+        
+        const threadid = (type == 'topic' || type == 'home') ? ssr[3] : ""||"";
+       
+        const layoutNumber = ((type == 'topic' || type == 'home') ? ssr[4] : type == 'solo' ? ssr[3] : ssr[2]) || "l1";
+       
+        const navTab = ((type == 'newsline') ? ssr[3] : type == "solo" ? ssr[4] : 0) || 1;
+       
+        const cc = (type == 'topic' || type == 'home') ? ssr[5] : ''||'';
+        
+        const ua = context.req.headers['user-agent'];
+        
         const bowser = Bowser.getParser(ua ? ua : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
 
         const platformType = bowser.getPlatformType();
-        // console.log('PLATFORM:', platformType);
-        let defaultWidth = 600;
-        if (platformType == 'tablet')
-            defaultWidth = 900;
-        else if (platformType == 'desktop')
-            defaultWidth = 1200;
+        
+        const defaultWidth = platformType == 'tablet'?900:platformType == 'desktop'?1200:600;
+       
         const botInfo=isbot({ua});
-        console.log("ISBOT==>",botInfo.bot);
-        const sourceDomainsString = process.env.SOURCE_DOMAINS || '';
-        const sourceDomains = sourceDomainsString.split(',');
-
+        
+        //redirect from legacy domains:
         if (host != process.env.CANONIC_DOMAIN) {
-            console.log('host not eual canonic_domain')
             if (type == 'topic' || type == 'home') {
                 return {
                     redirect: {
@@ -136,39 +130,12 @@ export const getServerSideProps = withSessionSsr(
                     },
                     props: {},
                 };
-
             }
         }
-        /*   sourceDomains.forEach((sd: string) => {
-               console.log("SOURCE DOMAINS: ", sd, host)
-               if (host.indexOf(sd) >= 0) {
-                   if (type == 'topic' || type == 'home') {
-                       return {
-                           redirect: {
-                               permanent: true,
-                               destination: `https://${process.env.CANONIC_DOMAIN}/${forum}/home/${tag}/${threadid}`,
-                           },
-                           props: {},
-                       };
-                   }
-                   else {
-                       return {
-                           redirect: {
-                               permanent: true,
-                               destination: `https://${process.env.CANONIC_DOMAIN}`,
-                           },
-                           props: {},
-                       };
-   
-                   }
-               }
-   
-           }); */
 
         // get encrypted session from the cookie or initialize the default   
         let startoptions = context.req.session?.options || null;
-       // console.log("SSR. gpt session options", startoptions)
-
+      
         if (!startoptions) {
             var randomstring = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
             startoptions = {
@@ -182,25 +149,20 @@ export const getServerSideProps = withSessionSsr(
                 userslug: '',
                 band: 1,
                 dark: -1,
-
             }
             context.req.session.options = startoptions;
-
             await context.req.session.save();
         }
         let options: Options = startoptions;
 
         if (options && !options.width)
             options.width = defaultWidth;
+
         if (typeof options.userslug === 'undefined')
             options.userslug = '';
 
-        // console.log("Options:", options)
-       
-        // TBA pre-fetching data for SSR, for now all data fetched client-side   
         const newsline = process.env.DEFAULT_NEWSLINE||'qwiket';
-
-        // console.log("SSR SESSION",options)
+        
         const qparams = {
             custom: true,
             forum,
@@ -215,25 +177,21 @@ export const getServerSideProps = withSessionSsr(
             isbot:botInfo.bot,
             isfb:botInfo.fb
         }
-        const isFirstServerCall = context.req?.url?.indexOf('/_next/data/') === -1
-        console.log("IS FIRST SERVER CALL=",isFirstServerCall)
+        
+        // useful for debugging:
+        //const isFirstServerCall = context.req?.url?.indexOf('/_next/data/') === -1
+        
+        //Disqus OAuth callback:
         if (code) {
-            console.log("CODE: ", code, state);
             const jsonState = JSON.parse(state);
             const { href } = jsonState;
-            console.log("CODE: ", code, state, href);
             const user = await processLoginCode(code, host);
             if (user) {
                 const { slug } = user;
-                console.log("ffff1")
-                //  axios.post(`/api/session/save`, { session: { userslug: slug } });
                 if (context.req.session.options) {
-                    console.log("context.req.session.option")
                     context.req.session.options.userslug = slug;
                     await context.req.session.save();
-
                     const userSession = await initLoginSession(slug, options)
-
                     if (userSession) {
                         options = userSession || options;
                         if (options)
@@ -241,7 +199,6 @@ export const getServerSideProps = withSessionSsr(
                         await context.req.session.save();
                     }
                 }
-                console.log("redirecting after code", href)
                 return {
                     redirect: {
                         permanent: true,
@@ -250,39 +207,33 @@ export const getServerSideProps = withSessionSsr(
                     props: {},
                 };
             }
-
-            //context.res.writeHeader(301,{Location:href} );
         }
         else if (options.userslug) {
             const userSession = await getUserSession(options.userslug)
             if (userSession && !shallowEqual(options, userSession)) {
                 options = userSession;
-                console.log("USER SESSION IS DIFFERENT FROM LOCAL")
                 if (options)
                     context.req.session.options = options;
                 await context.req.session.save();
             }
         }
 
+        // get channel meta data and layout:
         const channelConfig = await fetchChannelConfig(newsline);
-
-        console.log("GOT channelConfig",newsline, channelConfig)
-        const layoutType = type == 'topic' || type == 'home' ? 'context' : type == 'solo' ? 'newsline' : type;
+        
         const newslineKey: fetchChannelLayoutKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'newsline', options.dense, options.thick, layoutNumber];
+        
         const contextKey: fetchChannelLayoutKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'context', options.dense, options.thick, layoutNumber];
         
-        console.log("CALLING fetchChannelLayout:",newslineKey);
         const newslineLayout = await fetchChannelLayout(newslineKey);
-         console.log("GOT CHANNEL LAYOUT", JSON.stringify(newslineLayout))
-        console.log("=================")
+       
+        //pre-fill stale data for the client for all permutations:
         const newslineStaleLKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug,'newsline', 0, 1, layoutNumber];
         const newslineStaleLKey1 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'newsline', 0, 0, layoutNumber];
         const newslineStaleLKey2 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'newsline', 1, 0, layoutNumber];
         const newslineStaleLKey3 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'newsline', 1, 1, layoutNumber];
 
         const contextLayout = await fetchChannelLayout(contextKey);
-        // console.log("GOT CHANNEL LAYOUT", JSON.stringify(channelLayout))
-        // console.log("=================")
         const contextStaleLKey = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug,'context', 0, 1, layoutNumber];
         const contextStaleLKey1 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'context', 0, 0, layoutNumber];
         const contextStaleLKey2 = ['channelLayout', newsline, options.hasLayout, options.sessionid, options.userslug, 'context', 1, 0, layoutNumber];
@@ -304,8 +255,8 @@ export const getServerSideProps = withSessionSsr(
             [unstable_serialize(contextStaleLKey3)]: contextLayout,
 
             [unstable_serialize(['user', options ? options.userslug : ''])]: user
-        }
-
+        }        
+        //setup metadata for og: tags
         let meta: {
             description?: string,
             title?: string,
@@ -314,20 +265,19 @@ export const getServerSideProps = withSessionSsr(
             publishedTime?: number,
             url?: string,
             canonic?: string
-
         } = {};
+       
         if (type == 'topic' || type == 'home') {
+             //handle invalid topic slugs with 404 - google search requirement
             const check=threadid.split('-slug-');
             if(type=='topic'&&check.length<2&&threadid!='nro-is-moving-to-facebook-comments'){
                 context.res.statusCode = 404;
                 return { props: { error: 404 } }
-
             }
             const key:FetchTopicKey= {threadid:qparams.threadid,withBody:1,userslug:options.userslug,sessionid:options.sessionid,tag:qparams.tag,ackOverride:qparams.isbot||qparams.isfb};
-
             try {
                 const topic = await fetchTopic(key);
-               // console.log("GOT TOPIC:", JSON.stringify(topic))
+                //handle invalid slugs with 404:
                 if(!topic.success){
                     context.res.statusCode = 404;
                     return { props: { error: 404 } }
@@ -354,9 +304,8 @@ export const getServerSideProps = withSessionSsr(
             meta.title = channelConfig.channelDetails.displayName
             meta.image == channelConfig.channelDetails.logo;
             meta.publishedTime = Date.now() / 1000 | 0,
-                meta.canonic = `https://${process.env.CANONIC_DOMAIN}`
-        }
-        // console.log("META", type, JSON.stringify(meta))
+            meta.canonic = `https://${process.env.CANONIC_DOMAIN}`
+        }       
         const propsWrap = {
             props: {
                 session: options,
@@ -365,7 +314,5 @@ export const getServerSideProps = withSessionSsr(
                 meta
             }
         };
-
-        //  console.log("propsWrap",JSON.stringify({ propsWrap }))
-        return propsWrap;
+       return propsWrap;
     })

72fb563163c1324532f89731edd6126897e324bd
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-05-08 11:46:59 -0400

    Add copyright to summary

--- lib/encode-entities.ts
+++ lib/encode-entities.ts
@@ -1,4 +1,4 @@
-export default function encodeEntities(str) {
+export default function encodeEntities(str:string) {
     return str.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
         return '&#' + i.charCodeAt(0) + ';';
     });

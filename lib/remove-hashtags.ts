adfdaa4916993d50b008165e071f56a037aac2ec
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-10-17 11:33:01 -0400

    Update digest render.

--- lib/remove-hashtags.ts
+++ lib/remove-hashtags.ts
@@ -1,5 +1,9 @@
 export default  function removeHashtags(input: string): string {
     // This regular expression matches hashtags.
-    const hashtagRegex = /#\w+/g;
+    //const hashtagRegex = /#\w+/g;
+    const hashtagRegex = /#(\w+)\b/g;
+
+    const ret=input.replace(hashtagRegex, '');
+    console.log("removeHashtags:",input,ret);
     return input.replace(hashtagRegex, '');
 }
